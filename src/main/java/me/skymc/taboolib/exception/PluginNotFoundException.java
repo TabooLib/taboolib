3130ed8c6cdee603e16ee42352bf9493e145f1a3
坏黑 <sky@asgard.me>
2018-03-11 14:40:12 +0800

    版本更新至：3.78
    调整：数据保存提示现在默认关闭了
    新增：Language2.yml 新增 [book] 类型示范

---
+++ src/main/java/me/skymc/taboolib/exception/PluginNotFoundException.java
@@ -0,0 +1,10 @@
+package me.skymc.taboolib.exception;
+
+public class PluginNotFoundException extends Error {
+
+	private static final long serialVersionUID = -475540326950009346L;
+
+	public PluginNotFoundException(String message) {
+		super(message);
+	}
+}
