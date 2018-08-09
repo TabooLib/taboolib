4fba5a596b1190ecc1947a9439180f5c662c8686
坏黑 <sky@asgard.me>
2018-08-09 11:58:31 +0800

    挖了个天坑。

---
+++ src/main/java/me/skymc/taboolib/bungee/TabooLibBungee.java
@@ -0,0 +1,15 @@
+package me.skymc.taboolib.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @Author sky
+ * @Since 2018-08-09 11:47
+ */
+public class TabooLibBungee extends Plugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
