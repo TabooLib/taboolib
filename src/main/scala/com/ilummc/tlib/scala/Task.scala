486711a788fedc0e2f4013866afc8196906aee67
Izzel_Aliz <csh2001331@126.com>
2018-10-02 11:17:12 +0800

    scala api change

--- src/main/scala/com/ilummc/tlib/scala/Task.scala
+++ src/main/scala/com/ilummc/tlib/scala/Task.scala
@@ -4,15 +4,15 @@ import org.bukkit.plugin.Plugin
 
 object Task {
 
-  def apply(task: => Unit)(implicit plugin: Plugin): Int = {
+  def apply(task: => Any)(implicit plugin: Plugin): Int = {
     ScalaTaskExecutor(task).runTask(plugin).getTaskId
   }
 
-  def apply(delay: Long)(task: => Unit)(implicit plugin: Plugin): Int = {
+  def apply(delay: Long)(task: => Any)(implicit plugin: Plugin): Int = {
     ScalaTaskExecutor(task).runTaskLater(plugin, delay).getTaskId
   }
 
-  def apply(init: Long, period: Long)(task: => Unit)(implicit plugin: Plugin): Int = {
+  def apply(init: Long, period: Long)(task: => Any)(implicit plugin: Plugin): Int = {
     ScalaTaskExecutor(task).runTaskTimer(plugin, init, period).getTaskId
   }
 
