639234ec9fe05f517e0e40112cecf66456d293ea
坏黑 <sky@asgard.me>
2019-07-06 23:34:32 +0800

    Remove update-check

--- src/main/scala/me/skymc/taboolib/listener/ListenerPlayerJoinAndQuit.java
+++ src/main/scala/me/skymc/taboolib/listener/ListenerPlayerJoinAndQuit.java
@@ -1,11 +1,8 @@
 package me.skymc.taboolib.listener;
 
-import com.ilummc.tlib.resources.TLocale;
 import me.skymc.taboolib.Main;
-import me.skymc.taboolib.TabooLib;
 import me.skymc.taboolib.playerdata.DataUtils;
 import me.skymc.taboolib.translateuuid.TranslateUUID;
-import me.skymc.taboolib.update.UpdateTask;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,9 +15,6 @@ public class ListenerPlayerJoinAndQuit implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        if (UpdateTask.isHaveUpdate() && e.getPlayer().hasPermission("taboolib.update.notify")) {
-            TLocale.Logger.sendTo(e.getPlayer(), "UPDATETASK.VERSION-OUTDATED", String.valueOf(TabooLib.getPluginVersion()), String.valueOf(UpdateTask.getNewVersion()));
-        }
         if (TranslateUUID.isEnabled()) {
             Bukkit.getScheduler().runTaskAsynchronously(Main.getInst(), () -> TranslateUUID.updateUsername(e.getPlayer().getUniqueId(), e.getPlayer().getName()));
         }
