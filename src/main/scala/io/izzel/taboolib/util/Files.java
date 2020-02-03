cd43e736c9feef96cb0a724c0847ac42e0619134
sky <sky@asgard.me>
2020-02-03 18:40:13 +0800

    Update JDK 11

--- src/main/scala/io/izzel/taboolib/util/Files.java
+++ src/main/scala/io/izzel/taboolib/util/Files.java
@@ -319,6 +319,15 @@ public class Files {
         }
     }
 
+    public static void write(File file, WriteHandle writeHandle, boolean append) {
+        try (FileWriter fileWriter = new FileWriter(file, append); BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            writeHandle.write(bufferedWriter);
+            bufferedWriter.flush();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
     public static void write(OutputStream out, WriteHandle writeHandle) {
         try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out); BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)) {
             writeHandle.write(bufferedWriter);
