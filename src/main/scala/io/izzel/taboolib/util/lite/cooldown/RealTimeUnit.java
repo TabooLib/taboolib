7f8c82e6ee7bdee909f451f90f96869c3410cfc9
sky <sky@asgard.me>
2020-02-18 14:45:53 +0800

    Update

---
+++ src/main/scala/io/izzel/taboolib/util/lite/cooldown/RealTimeUnit.java
@@ -0,0 +1,24 @@
+package io.izzel.taboolib.util.lite.cooldown;
+
+/**
+ * @Author sky
+ * @Since 2020-02-18 14:16
+ */
+public enum RealTimeUnit {
+
+    /**
+     * 以时为单位
+     */
+    HOUR,
+
+    /**
+     * 以日为单位
+     */
+    DAY,
+
+    /**
+     * 以周为单位
+     */
+    WEEK
+
+}
