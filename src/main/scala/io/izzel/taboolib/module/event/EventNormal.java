12ea70a65a530598a656e499f1a5769371842d32
sky <sky@asgard.me>
2020-12-19 16:32:32 +0800

    Update Items

--- src/main/scala/io/izzel/taboolib/module/event/EventNormal.java
+++ src/main/scala/io/izzel/taboolib/module/event/EventNormal.java
@@ -1,12 +1,9 @@
 package io.izzel.taboolib.module.event;
 
-import io.izzel.taboolib.util.Ref;
-import io.izzel.taboolib.util.Reflection;
+import io.izzel.taboolib.kotlin.Reflex;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-
-import java.lang.reflect.Field;
 
 /**
  * 普通监听器简化接口
@@ -33,12 +30,7 @@ public abstract class EventNormal<T extends EventNormal> extends Event {
     }
 
     public T async(boolean value) {
-        try {
-            Field asyncField = Reflection.getField(Event.class, true, "async");
-            Ref.putField(this, asyncField, value);
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        Reflex.Companion.from(Event.class, this).write("async", value);
         return (T) this;
     }
 
