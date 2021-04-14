807e2b07a2f7f51ba62b357b6a0a430028e8c3ca
sky <sky@asgard.me>
2021-04-14 20:45:40 +0800

    5.61

--- src/main/scala/io/izzel/taboolib/module/db/sql/query/RunnableUpdate.java
+++ src/main/scala/io/izzel/taboolib/module/db/sql/query/RunnableUpdate.java
@@ -5,6 +5,11 @@ import io.izzel.taboolib.module.locale.logger.TLogger;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @author sky
@@ -15,6 +20,9 @@ public class RunnableUpdate {
     private final String query;
     private DataSource dataSource;
     private TaskStatement statement;
+    private Consumer<Connection> connectionFinish;
+    private Consumer<Statement> statementFinish;
+    private Consumer<Integer> result;
 
     public RunnableUpdate(String query) {
         this.query = query;
@@ -30,13 +38,37 @@ public class RunnableUpdate {
         return this;
     }
 
+    public RunnableUpdate connectionFinish(Consumer<Connection> consumer) {
+        this.connectionFinish = consumer;
+        return this;
+    }
+
+    public RunnableUpdate statementFinish(Consumer<Statement> consumer) {
+        this.statementFinish = consumer;
+        return this;
+    }
+
+    public RunnableUpdate result(Consumer<Integer> consumer) {
+        this.result = consumer;
+        return this;
+    }
+
     public void run() {
         if (dataSource != null) {
             try (Connection connection = dataSource.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
                 if (statement != null) {
                     statement.execute(preparedStatement);
                 }
-                preparedStatement.executeUpdate();
+                int i = preparedStatement.executeUpdate();
+                if (result != null) {
+                    result.accept(i);
+                }
+                if (statementFinish != null) {
+                    statementFinish.accept(preparedStatement);
+                }
+                if (connectionFinish != null) {
+                    connectionFinish.accept(connection);
+                }
             } catch (Exception e) {
                 TLogger.getGlobalLogger().error("An exception occurred in the database. (" + query + ")");
                 TLogger.getGlobalLogger().error("Reason: " + e.toString());
