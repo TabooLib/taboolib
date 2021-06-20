786aeece28c6774e0ba5462a1141a7160198eade
sky <sky@asgard.me>
2021-06-20 22:10:08 +0800

    + update bukkit dependency

--- module-porticus/build.gradle.kts
+++ module-porticus/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 repositories {
+    maven {
+        isAllowInsecureProtocol = true
+        url = uri("http://repo.ptms.ink/repository/maven-releases/")
+    }
     maven { url = uri("https://maven.aliyun.com/repository/central") }
     maven { url = uri("https://repo.codemc.io/repository/maven-public/") }
     maven { url = uri("https://repo.codemc.io/repository/nms/") }
@@ -14,7 +18,7 @@ repositories {
 dependencies {
     compileOnly("com.github.ben-manes.caffeine:caffeine:3.0.2")
     compileOnly("net.md-5:bungeecord-bootstrap:1.17-R0.1-20210614.231150-8@jar")
-    compileOnly("org.spigotmc:spigot:1.17-R0.1-20210612.142052-2")
+    compileOnly("ink.ptms.core:v11605:11605")
     compileOnly(project(":common"))
     compileOnly(project(":module-dependency"))
     compileOnly(kotlin("stdlib"))
