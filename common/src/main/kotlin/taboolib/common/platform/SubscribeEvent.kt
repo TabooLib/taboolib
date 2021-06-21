afc0bc1b4c421a30fecd580f6b5cb753f2e3245b
sky <sky@asgard.me>
2021-06-21 18:40:07 +0800

    + update event api

--- common/src/main/kotlin/taboolib/common/platform/SubscribeEvent.kt
+++ common/src/main/kotlin/taboolib/common/platform/SubscribeEvent.kt
@@ -3,9 +3,11 @@ package taboolib.common.platform
 @Target(AnnotationTarget.FUNCTION)
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class SubscribeEvent(
-    val priority: EventPriority = EventPriority.NORMAL, val ignoreCancelled: Boolean = false,
+    val priority: EventPriority = EventPriority.NORMAL,
+    val ignoreCancelled: Boolean = false,
     // only bungeecord platform
     val level: Int = -1,
     // only sponge platform
-    val order: EventOrder = EventOrder.DEFAULT, val beforeModifications: Boolean = false,
+    val order: EventOrder = EventOrder.DEFAULT,
+    val beforeModifications: Boolean = false,
 )
