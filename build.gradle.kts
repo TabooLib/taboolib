b6e9a5b558914009a075a257631971a9972136c3
sky <sky@asgard.me>
2021-06-27 00:50:12 +0800

    + update plugin platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,43 @@
+plugins {
+    `maven-publish`
+}
+
 allprojects {
     group = "taboolib"
-    version = "6.0.0"
+    version = "test-600-0"
 
-    tasks.withType(Jar::class.java) {
+    tasks.withType<Jar> {
         destinationDirectory.set(file("$rootDir/build/libs"))
     }
-
-    tasks.withType(JavaCompile::class.java) {
+    tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://repo2s.ptms.ink/repository/maven-releases/")
+            credentials {
+                username = project.findProperty("user").toString()
+                password = project.findProperty("password").toString()
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.izzel"
+            artifactId = "taboolib"
+            version = project.version.toString()
+            // subprojects -> classifier
+            file("$buildDir/libs").listFiles()?.forEach { file ->
+                artifact(file) {
+                    classifier = file.nameWithoutExtension.substring(0, file.nameWithoutExtension.length - version.length - 1)
+                }
+            }
+        }
     }
 }
