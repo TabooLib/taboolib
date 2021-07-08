3e7d8a5bc15ff42f2b054cff1d91d502309a7924
sky <sky@asgard.me>
2021-07-08 15:22:47 +0800

    + update javadoc

--- common/src/main/java/taboolib/common/OpenContainer.java
+++ common/src/main/java/taboolib/common/OpenContainer.java
@@ -11,9 +11,28 @@ import org.jetbrains.annotations.NotNull;
  */
 public interface OpenContainer {
 
+    /**
+     * 插件名字
+     *
+     * @return {@link String}
+     */
     String getName();
 
+    /**
+     * 注册开放接口
+     *
+     * @param name 名字
+     * @param any  序列化类
+     * @param args 参数
+     */
     void register(String name, @NotNull byte[] any, @NotNull String[] args);
 
+    /**
+     * 注销开放接口
+     *
+     * @param name 名字
+     * @param any  序列化类
+     * @param args 参数
+     */
     void unregister(String name, @NotNull byte[] any, @NotNull String[] args);
 }
--- common/src/main/java/taboolib/common/OpenReceiver.java
+++ common/src/main/java/taboolib/common/OpenReceiver.java
@@ -11,7 +11,23 @@ import org.jetbrains.annotations.NotNull;
  */
 public interface OpenReceiver {
 
+    /**
+     * 注册开放接口
+     *
+     * @param name 名字
+     * @param any  序列化类
+     * @param args 参数
+     * @return boolean 是否注册成功
+     */
     boolean register(@NotNull String name, @NotNull byte[] any, @NotNull String[] args);
 
+    /**
+     * 注销开放接口
+     *
+     * @param name 名字
+     * @param any  序列化类
+     * @param args 参数
+     * @return boolean 是否注销成功
+     */
     boolean unregister(@NotNull String name, @NotNull byte[] any, @NotNull String[] args);
 }
