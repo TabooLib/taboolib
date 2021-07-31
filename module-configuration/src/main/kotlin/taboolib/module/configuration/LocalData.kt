ada79fb551e9f5a164a479270015623f7be420bc
坏黑 <b@skymc.me>
2021-07-31 20:21:09 +0800

    + [pre16] fix local

--- module-configuration/src/main/kotlin/taboolib/module/configuration/LocalData.kt
+++ module-configuration/src/main/kotlin/taboolib/module/configuration/LocalData.kt
@@ -4,26 +4,27 @@ package taboolib.module.configuration
 
 import taboolib.common.Isolated
 import taboolib.common.LifeCycle
+import taboolib.common.io.newFile
 import taboolib.common.platform.Awake
 import taboolib.common.platform.getDataFolder
 import taboolib.common.platform.submit
 import java.io.File
 import java.util.concurrent.ConcurrentHashMap
 
-private var created = false
+private var init = true
 private val files = ConcurrentHashMap<String, SecuredFile>()
 
 fun createLocal(path: String): SecuredFile {
     if (files.containsKey(path)) {
         return files[path]!!
     }
-    if (!created) {
-        created = true
+    if (init) {
+        init = false
         submit(period = 1200, async = true) {
             Local.saveAll()
         }
     }
-    return files.computeIfAbsent(path) { SecuredFile.loadConfiguration(File(getDataFolder(), path)) }
+    return files.computeIfAbsent(path) { SecuredFile.loadConfiguration(newFile(getDataFolder(), path)) }
 }
 
 @Isolated
