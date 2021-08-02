5e5bb4c3c52e353cded445fbb8ce3f013df1b1ce
Score2 <836696023@qq.com>
2021-08-02 19:56:05 +0800

    Update commons-lang to 3.12.0.

--- module-kether/build.gradle.kts
+++ module-kether/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     compileOnly("public:PlaceholderAPI:2.10.9")
     compileOnly("ink.ptms.core:v11200:11200:all")
     compileOnly("com.google.guava:guava:17.0")
-    compileOnly("org.apache.commons:commons-lang3:3.5")
+    compileOnly("org.apache.commons:commons-lang3:3.12.0")
     compileOnly(project(":common"))
     compileOnly(project(":common-5"))
     compileOnly(project(":module-chat"))
