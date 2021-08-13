73626103fbe926a4552a84eb783b11d2d6eb033f
ItsFlicker <1812496278@qq.com>
2021-08-13 14:46:39 +0800

    fix lang type Sound and Title

--- module-lang/build.gradle.kts
+++ module-lang/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-    compileOnly("io.izzel.kether:common:1.0.16")
     compileOnly(project(":common"))
     compileOnly(project(":common-5"))
     compileOnly(project(":module-chat"))
