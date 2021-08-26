e676a2b04aec67e5d15e67d3089be762352dd6ce
Ling_Haner <Ling_Haner@163.com>
2021-08-26 14:35:13 +0800

    优化getMap,新增getLocation,setLocation

--- module-configuration/src/main/kotlin/taboolib/module/configuration/ConfigUtil.kt
+++ module-configuration/src/main/kotlin/taboolib/module/configuration/ConfigUtil.kt
@@ -1,16 +1,45 @@
 package taboolib.module.configuration
 
+import taboolib.common.util.Location
 import taboolib.library.configuration.ConfigurationSection
 
 @Suppress("UNCHECKED_CAST")
 fun <K, V> ConfigurationSection.getMap(path: String): Map<K, V> {
     val map = mutableMapOf<K, V>()
-    getConfigurationSection(path).getKeys(false).forEach { key ->
-        try {
-            map[key as K] = get("${path}.${key}") as V
-        } catch (ex: Throwable) {
-            ex.printStackTrace()
+    getConfigurationSection(path)?.let { section ->
+        section.getKeys(false).forEach { key ->
+            try {
+                map[key as K] = section.get(key) as V
+            } catch (ex: Throwable) {
+                ex.printStackTrace()
+            }
         }
     }
     return map
+}
+
+fun ConfigurationSection.getLocation(path: String): Location? {
+    val section = getConfigurationSection(path) ?: return null
+    getConfigurationSection(path)?.let { section ->
+        return Location(
+            section.getString("world"),
+            section.getDouble("x"),
+            section.getDouble("y"),
+            section.getDouble("z"),
+            section.getDouble("pitch").toFloat(),
+            section.getDouble("yaw").toFloat()
+        )
+
+    } ?: return null
+}
+
+fun ConfigurationSection.setLocation(path: String, location: Location) {
+    createSection(path).apply {
+        set("world",location.world)
+        set("x",location.x)
+        set("y",location.y)
+        set("z",location.z)
+        set("pitch",location.pitch)
+        set("yaw",location.yaw)
+    }
 }
