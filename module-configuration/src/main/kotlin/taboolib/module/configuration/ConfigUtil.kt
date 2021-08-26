d55591159c9861c6fdd4effeb6775f1bae036004
Ling_Haner <Ling_Haner@163.com>
2021-08-26 18:53:05 +0800

    优化getMap,新增getLocation,setLocation

--- module-configuration/src/main/kotlin/taboolib/module/configuration/ConfigUtil.kt
+++ module-configuration/src/main/kotlin/taboolib/module/configuration/ConfigUtil.kt
@@ -1,5 +1,8 @@
+@file:Isolated
+
 package taboolib.module.configuration
 
+import taboolib.common.Isolated
 import taboolib.common.util.Location
 import taboolib.library.configuration.ConfigurationSection
 
@@ -28,17 +31,16 @@ fun ConfigurationSection.getLocation(path: String): Location? {
             section.getDouble("pitch").toFloat(),
             section.getDouble("yaw").toFloat()
         )
-
     } ?: return null
 }
 
 fun ConfigurationSection.setLocation(path: String, location: Location) {
     createSection(path).apply {
-        set("world",location.world)
-        set("x",location.x)
-        set("y",location.y)
-        set("z",location.z)
-        set("pitch",location.pitch)
-        set("yaw",location.yaw)
+        set("world", location.world)
+        set("x", location.x)
+        set("y", location.y)
+        set("z", location.z)
+        set("pitch", location.pitch)
+        set("yaw", location.yaw)
     }
 }
