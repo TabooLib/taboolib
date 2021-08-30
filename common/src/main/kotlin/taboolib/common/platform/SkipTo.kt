d058ac90e00bae9798a1e27f52515f1213ac1563
坏黑 <b@skymc.me>
2021-08-30 22:29:21 +0800

    + [6.0.0] update SkipTo

--- common/src/main/kotlin/taboolib/common/platform/SkipTo.kt
+++ common/src/main/kotlin/taboolib/common/platform/SkipTo.kt
@@ -2,6 +2,6 @@ package taboolib.common.platform
 
 import taboolib.common.LifeCycle
 
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class SkipTo(val value: LifeCycle = LifeCycle.CONST)
--- common/src/main/kotlin/taboolib/common/platform/function/Adapter.kt
+++ common/src/main/kotlin/taboolib/common/platform/function/Adapter.kt
@@ -6,26 +6,44 @@ import taboolib.common.platform.ProxyPlayer
 import taboolib.common.platform.service.PlatformAdapter
 import java.util.*
 
+/**
+ * 获取控制台
+ */
 fun console(): ProxyCommandSender {
     return PlatformFactory.getService<PlatformAdapter>().console()
 }
 
+/**
+ * 将平台实现转换为跨平台实现
+ */
 fun adaptCommandSender(any: Any): ProxyCommandSender {
     return PlatformFactory.getService<PlatformAdapter>().adaptCommandSender(any)
 }
 
+/**
+ * 获取所有在线玩家
+ */
 fun onlinePlayers(): List<ProxyPlayer> {
     return PlatformFactory.getService<PlatformAdapter>().onlinePlayers()
 }
 
+/**
+ * 将平台实现转换为跨平台实现
+ */
 fun adaptPlayer(any: Any): ProxyPlayer {
     return PlatformFactory.getService<PlatformAdapter>().adaptPlayer(any)
 }
 
+/**
+ * 通过名称获取玩家
+ */
 fun getProxyPlayer(name: String): ProxyPlayer? {
     return onlinePlayers().firstOrNull { it.name == name }
 }
 
+/**
+ * 通过 UUID 获取玩家
+ */
 fun getProxyPlayer(uuid: UUID): ProxyPlayer? {
     return onlinePlayers().firstOrNull { it.uniqueId == uuid }
 }
