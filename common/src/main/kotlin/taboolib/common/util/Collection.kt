279ac38fb2844375584404b58d4e19c0d3bd1f5a
坏黑 <b@skymc.me>
2021-08-31 02:09:42 +0800

    + [6.0.0] fix Collection.kt

--- common/src/main/kotlin/taboolib/common/util/Collection.kt
+++ common/src/main/kotlin/taboolib/common/util/Collection.kt
@@ -8,14 +8,14 @@ fun Any.asList(): List<String> {
 }
 
 fun <T> MutableList<T>.setSafely(index: Int, element: T, def: T) {
-    while (size <= index) {
+    while (index >= size) {
         add(def)
     }
     this[index] = element
 }
 
 fun <T> MutableList<T>.addSafely(index: Int, element: T, def: T) {
-    while (size <= index) {
+    while (index >= size) {
         add(def)
     }
     add(index, element)
