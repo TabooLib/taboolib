215d69bffbad59a706ce81ccaac583af2a2691d7
坏黑 <b@skymc.me>
2021-11-09 17:14:33 +0800

    [6.0.4] Update Kether

--- module/module-kether/src/main/kotlin/taboolib/module/kether/action/loop/ActionFor.kt
+++ module/module-kether/src/main/kotlin/taboolib/module/kether/action/loop/ActionFor.kt
@@ -15,30 +15,39 @@ class ActionFor(val key: String, val values: ParsedAction<*>, val action: Parsed
         frame.newFrame(values).run<Any>().thenApply {
             fun process(cur: Int, i: List<Any>) {
                 if (cur < i.size) {
-                    frame.variables()[key] = i[cur]
+                    val el = i[cur]
+                    if (el is Map.Entry<*, *>) {
+                        frame.variables()["$key-key"] = el.key
+                        frame.variables()["$key-value"] = el.value
+                    }
+                    frame.variables()[key] = el
                     frame.newFrame(action).run<Any>().thenApply {
                         if (frame.script().breakLoop) {
                             frame.script().breakLoop = false
+                            frame.variables().also { v ->
+                                v.remove(key)
+                                v.remove("$key-key")
+                                v.remove("$key-value")
+                            }
                             future.complete(null)
                         } else {
                             process(cur + 1, i)
                         }
                     }
                 } else {
-                    frame.variables().remove(key)
+                    frame.variables().also { v ->
+                        v.remove(key)
+                        v.remove("$key-key")
+                        v.remove("$key-value")
+                    }
                     future.complete(null)
                 }
             }
             when (it) {
-                is Collection<*> -> {
-                    process(0, it.map { i -> i as Any }.toList())
-                }
-                is Array<*> -> {
-                    process(0, it.map { i -> i as Any }.toList())
-                }
-                else -> {
-                    process(0, listOf(it))
-                }
+                is Collection<*> -> process(0, it.map { i -> i as Any }.toList())
+                is Array<*> -> process(0, it.map { i -> i as Any }.toList())
+                is Map<*, *> -> process(0, it.entries.toList())
+                else -> process(0, listOf(it))
             }
         }
         return future
--- module/module-kether/src/main/kotlin/taboolib/module/kether/action/loop/ActionMap.kt
+++ module/module-kether/src/main/kotlin/taboolib/module/kether/action/loop/ActionMap.kt
@@ -16,34 +16,42 @@ class ActionMap(val key: String, val values: ParsedAction<*>, val action: Parsed
         frame.newFrame(values).run<Any>().thenApply {
             fun process(cur: Int, i: List<Any>) {
                 if (cur < i.size) {
-                    frame.variables()[key] = i[cur]
+                    val el = i[cur]
+                    if (el is Map.Entry<*, *>) {
+                        frame.variables()["$key-key"] = el.key
+                        frame.variables()["$key-value"] = el.value
+                    }
+                    frame.variables()[key] = el
                     frame.newFrame(action).run<Any>().thenApply { map ->
                         if (map != null) {
                             result += map
                         }
                         if (frame.script().breakLoop) {
                             frame.script().breakLoop = false
-                            frame.variables().remove(key)
+                            frame.variables().also { v ->
+                                v.remove(key)
+                                v.remove("$key-key")
+                                v.remove("$key-value")
+                            }
                             future.complete(result)
                         } else {
                             process(cur + 1, i)
                         }
                     }
                 } else {
-                    frame.variables().remove(key)
+                    frame.variables().also { v ->
+                        v.remove(key)
+                        v.remove("$key-key")
+                        v.remove("$key-value")
+                    }
                     future.complete(result)
                 }
             }
             when (it) {
-                is Collection<*> -> {
-                    process(0, it.map { i -> i as Any }.toList())
-                }
-                is Array<*> -> {
-                    process(0, it.map { i -> i as Any }.toList())
-                }
-                else -> {
-                    process(0, listOf(it))
-                }
+                is Collection<*> -> process(0, it.map { i -> i as Any }.toList())
+                is Array<*> -> process(0, it.map { i -> i as Any }.toList())
+                is Map<*, *> -> process(0, it.entries.toList())
+                else -> process(0, listOf(it))
             }
         }
         return future
