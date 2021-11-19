78bd5c5765b49ada2c111b056a210aa194a8319b
Score2 <836696023@qq.com>
2021-11-19 20:01:31 +0800

    Update gradle version. Fixed fail build.

--- platform/platform-sponge-api9/build.gradle.kts
+++ platform/platform-sponge-api9/build.gradle.kts
@@ -1,8 +1,13 @@
 repositories {
-    maven("https://repo.spongepowered.org/maven")
+    maven { url = uri("https://repo.spongepowered.org/maven") }
 }
 
 dependencies {
     compileOnly("org.spongepowered:spongeapi:9.0.0-SNAPSHOT")
     compileOnly(project(":common"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
 }
