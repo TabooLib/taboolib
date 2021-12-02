502df735026a313e7677d24a2418bb1f0748162d
坏黑 <b@skymc.me>
2021-12-02 17:27:00 +0800

    [6.0.6] Fix ConfigInjector.kt

--- module/module-configuration/src/main/kotlin/taboolib/module/configuration/ConfigInjector.kt
+++ module/module-configuration/src/main/kotlin/taboolib/module/configuration/ConfigInjector.kt
@@ -21,8 +21,8 @@ import java.util.function.Supplier
     RuntimeDependency("!com.typesafe:config:1.4.1", test = "!com.typesafe.config.Config"),
     RuntimeDependency("!com.electronwill.night-config:core:3.6.5", test = "!com.electronwill.nightconfig.core.Config"),
     RuntimeDependency("!com.electronwill.night-config:toml:3.6.5", test = "!com.electronwill.nightconfig.toml.TomlFormat"),
-    RuntimeDependency("!com.electronwill.night-config:json:3.6.5", test = "!com.electronwill.nightconfig.toml.JsonFormat"),
-    RuntimeDependency("!com.electronwill.night-config:hocon:3.6.5", test = "!com.electronwill.nightconfig.toml.HoconFormat")
+    RuntimeDependency("!com.electronwill.night-config:json:3.6.5", test = "!com.electronwill.nightconfig.json.JsonFormat"),
+    RuntimeDependency("!com.electronwill.night-config:hocon:3.6.5", test = "!com.electronwill.nightconfig.hocon.HoconFormat")
 )
 @Awake
 object ConfigInjector : Injector.Fields {
