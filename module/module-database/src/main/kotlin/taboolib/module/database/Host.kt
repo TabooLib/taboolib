47b36286aa4295db81e9a1ca42ed8bf3dac423bc
坏黑 <b@skymc.me>
2021-12-06 00:08:20 +0800

    [6.0.6] Update DataSource release

--- module/module-database/src/main/kotlin/taboolib/module/database/Host.kt
+++ module/module-database/src/main/kotlin/taboolib/module/database/Host.kt
@@ -1,5 +1,9 @@
 package taboolib.module.database
 
+import com.zaxxer.hikari.HikariDataSource
+import taboolib.common.LifeCycle
+import taboolib.common.platform.Awake
+import java.util.concurrent.CopyOnWriteArrayList
 import javax.sql.DataSource
 
 /**
@@ -14,7 +18,21 @@ abstract class Host<T : ColumnBuilder> {
 
     abstract val connectionUrlSimple: String?
 
-    fun createDataSource(): DataSource {
-        return Database.createDataSource(this)
+    fun createDataSource(autoRelease: Boolean = true): DataSource {
+        return Database.createDataSource(this).also {
+            if (autoRelease) {
+                dataSources += it as HikariDataSource
+            }
+        }
+    }
+
+    companion object {
+
+        internal val dataSources = CopyOnWriteArrayList<HikariDataSource>()
+
+        @Awake(LifeCycle.DISABLE)
+        internal fun release() {
+            dataSources.forEach { it.close() }
+        }
     }
 }
