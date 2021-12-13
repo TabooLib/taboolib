8a2bded3f13fc24d7c845c0cf3cee0cb6cef9b77
Kylepoops <62238380+Kylepoops@users.noreply.github.com>
2021-12-13 08:54:32 +0800

    improve readability

--- common/src/main/kotlin/taboolib/common/io/File2.kt
+++ common/src/main/kotlin/taboolib/common/io/File2.kt
@@ -20,14 +20,16 @@ fun File.deepDeleteAsync(await: Boolean = false, futures: MutableSet<Future<*>>?
     // first submit the task and get the future
     val future = executor.submit {
         if (this.exists()) {
-            if (this.isDirectory) listFiles()?.let { files ->
-                // Construct another future set here
-                // Because we need all the subdirectories and files to be deleted before this directory is deleted
-                val thisFutures = mutableSetOf<Future<*>>()
-                // Pass the future set to this, so we can store all the future
-                files.forEach { it.deepDeleteAsync(futures = thisFutures) }
-                // Wait for all the subdirectories and files to be deleted
-                thisFutures.forEach(Future<*>::get)
+            if (this.isDirectory) {
+                listFiles()?.let { files ->
+                    // Construct another future set here
+                    // Because we need all the subdirectories and files to be deleted before this directory is deleted
+                    val thisFutures = mutableSetOf<Future<*>>()
+                    // Pass the future set to this, so we can store all the future
+                    files.forEach { it.deepDeleteAsync(futures = thisFutures) }
+                    // Wait for all the subdirectories and files to be deleted
+                    thisFutures.forEach(Future<*>::get)
+                }
             }
             // Finally, delete this file or directory
             this.delete()
