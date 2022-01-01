9828f8006779a64ef1dd9e8a1b41b40fe30b07c8
坏黑 <b@skymc.me>
2022-01-02 00:16:20 +0800

    [6.0.7] Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ subprojects {
 
 publishing {
     repositories {
-        maven("http://ptms.ink:8081/repository/releases") {
+        maven("http://repo.ptms.ink/repository/releases") {
             isAllowInsecureProtocol = true
             credentials {
                 username = project.findProperty("taboolibUsername").toString()
