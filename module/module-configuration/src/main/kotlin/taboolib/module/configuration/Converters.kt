c15cf98ba9f96f454ebf3d06370317e41edf5620
坏黑 <b@skymc.me>
2022-01-10 20:19:43 +0800

    [6.0.7] Update UUIDConverter

--- module/module-configuration/src/main/kotlin/taboolib/module/configuration/Converters.kt
+++ module/module-configuration/src/main/kotlin/taboolib/module/configuration/Converters.kt
@@ -2,7 +2,10 @@ package taboolib.module.configuration
 
 import com.electronwill.nightconfig.core.UnmodifiableConfig
 import com.electronwill.nightconfig.core.conversion.Converter
+import taboolib.common.Isolated
+import java.util.*
 
+@Isolated
 class MapConverter : Converter<Map<*, *>, UnmodifiableConfig> {
 
     override fun convertToField(config: UnmodifiableConfig): Map<*, *> {
@@ -11,5 +14,17 @@ class MapConverter : Converter<Map<*, *>, UnmodifiableConfig> {
 
     override fun convertFromField(map: Map<*, *>): UnmodifiableConfig {
         return (Configuration.fromMap(map) as ConfigSection).root
+    }
+}
+
+@Isolated
+class UUIDConverter : Converter<UUID, String> {
+
+    override fun convertToField(value: String): UUID {
+        return UUID.fromString(value)
+    }
+
+    override fun convertFromField(value: UUID): String {
+        return value.toString()
     }
 }
