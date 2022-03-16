0068f307a8b7f718357f584082f449b0ac5130b8
kirraObj <c@skymc.me>
2022-03-16 17:53:12 +0800

    Add some useful extension functions to platform-bukkit

--- platform/platform-bukkit/src/main/kotlin/taboolib/platform/util/BukkitDamage.kt
+++ platform/platform-bukkit/src/main/kotlin/taboolib/platform/util/BukkitDamage.kt
@@ -1,11 +1,18 @@
 @file:Isolated
+
 package taboolib.platform.util
 
+import org.bukkit.entity.Damageable
 import org.bukkit.entity.EvokerFangs
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Projectile
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.PlayerDeathEvent
 import taboolib.common.Isolated
+
+fun Damageable.kill() {
+    health = 0.0
+}
 
 val EntityDamageByEntityEvent.attacker: LivingEntity?
     get() = if (damager is LivingEntity) {
@@ -14,6 +21,13 @@ val EntityDamageByEntityEvent.attacker: LivingEntity?
         (damager as Projectile).shooter as LivingEntity?
     } else if (damager.javaClass.simpleName == "EvokerFangs" && damager is EvokerFangs) {
         (damager as EvokerFangs).owner
+    } else {
+        null
+    }
+
+val PlayerDeathEvent.killer: LivingEntity?
+    get() = if (entity.killer != null && entity.killer is LivingEntity) {
+        entity.killer as LivingEntity
     } else {
         null
     }
--- platform/platform-bukkit/src/main/kotlin/taboolib/platform/util/PlayerUtil.kt
+++ platform/platform-bukkit/src/main/kotlin/taboolib/platform/util/PlayerUtil.kt
@@ -4,8 +4,11 @@ package taboolib.platform.util
 
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.EquipmentSlot.*
 import org.bukkit.inventory.ItemStack
 import taboolib.common.Isolated
+import taboolib.common.platform.function.adaptPlayer
 
 fun Player.giveItem(itemStack: List<ItemStack>) {
     itemStack.forEach { giveItem(it) }
@@ -23,4 +26,32 @@ fun Player.getUsingItem(material: Material): ItemStack? {
         inventory.itemInOffHand.type == material -> inventory.itemInOffHand
         else -> null
     }
+}
+
+fun Player.sendActionBar(message: String) {
+    adaptPlayer(this).sendActionBar(message)
+}
+
+fun Player.getEquipmentItem(slot: EquipmentSlot = HAND): ItemStack? {
+    return when (slot) {
+        HAND -> inventory.itemInMainHand
+        OFF_HAND -> inventory.itemInOffHand
+        HEAD -> inventory.armorContents[0]
+        CHEST -> inventory.armorContents[1]
+        FEET -> inventory.armorContents[2]
+        LEGS -> inventory.armorContents[3]
+    }
+}
+
+fun Player.feed() {
+    foodLevel = 20
+}
+
+fun Player.saturate() {
+    saturation = 20F
+}
+
+fun Player.feedSaturate() {
+    feed()
+    saturate()
 }
