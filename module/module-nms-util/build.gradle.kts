f0b83182db83871d1fff79509246ae0bb6bf69a6
tony_all <admin@minicraf.top>
2022-07-06 15:58:18 +0800

    fix "unexpected null component" in Minecraft

--- module/module-nms-util/build.gradle.kts
+++ module/module-nms-util/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     compileOnly("ink.ptms:nms-all:1.0.0")
     compileOnly("ink.ptms.core:v11900:11900-minimize:mapped")
     compileOnly("ink.ptms.core:v11900:11900-minimize:universal")
+    compileOnly("com.mojang:brigadier:1.0.17")
     compileOnly(project(":common"))
     compileOnly(project(":module:module-nms"))
     compileOnly(project(":platform:platform-bukkit"))
