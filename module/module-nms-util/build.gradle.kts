0eb35e2b6b3f73fec5ed79430b6185593cc08960
坏黑 <b@skymc.me>
2022-07-25 13:25:12 +0800

    [6.0.9][Experimental-Update] Fix (recommend: 6.0.8-9)

--- module/module-nms-util/build.gradle.kts
+++ module/module-nms-util/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     compileOnly("com.google.guava:guava:21.0")
     compileOnly("com.google.code.gson:gson:2.8.7")
     compileOnly("ink.ptms:nms-all:1.0.0")
-    compileOnly("ink.ptms.core:v11800:11800-minimize:mapped")
     compileOnly("ink.ptms.core:v11900:11900-minimize:mapped")
     compileOnly("ink.ptms.core:v11900:11900-minimize:universal")
     compileOnly("com.mojang:brigadier:1.0.17")
