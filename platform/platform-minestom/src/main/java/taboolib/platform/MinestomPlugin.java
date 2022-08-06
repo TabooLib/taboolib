0552e7acfb349493fe64f04d4a247159cd2a69f8
Wishrem <wishrem02@gmail.com>
2022-08-06 07:56:17 -0700

    add new platform minestom

---
+++ platform/platform-minestom/src/main/java/taboolib/platform/MinestomPlugin.java
@@ -0,0 +1,16 @@
+package taboolib.platform;
+
+import net.minestom.server.extensions.Extension;
+
+public class MinestomPlugin extends Extension {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+}
