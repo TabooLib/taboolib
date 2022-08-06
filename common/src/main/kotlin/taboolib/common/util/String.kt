b3e500fece5de6c3e6025af2d2f5240b7ab67ca7
坏黑 <b@skymc.me>
2022-08-06 23:53:41 +0800

    [6.0.9][Experimental-Update] Update replaceWithOrder (recommend: 6.0.8-9)

--- common/src/main/kotlin/taboolib/common/util/String.kt
+++ common/src/main/kotlin/taboolib/common/util/String.kt
@@ -14,14 +14,24 @@ fun String.replaceWithOrder(vararg args: Any): String {
         val mark = i
         if (chars[i] == '{') {
             var num = 0
-            while (i + 1 < chars.size && Character.isDigit(chars[i + 1])) {
+            val alias = StringBuilder()
+            while (i + 1 < chars.size && chars[i + 1] != '}') {
                 i++
-                num *= 10
-                num += chars[i] - '0'
+                if (Character.isDigit(chars[i]) && alias.isEmpty()) {
+                    num *= 10
+                    num += chars[i] - '0'
+                } else {
+                    alias.append(chars[i])
+                }
             }
             if (i != mark && i + 1 < chars.size && chars[i + 1] == '}') {
                 i++
-                builder.append(args.getOrNull(num) ?: "{$num}")
+                if (alias.isNotEmpty()) {
+                    val str = alias.toString()
+                    builder.append((args.firstOrNull { it is Pair<*, *> && it.second == str } as? Pair<*, *>)?.first ?: "{$str}")
+                } else {
+                    builder.append(args.getOrNull(num) ?: "{$num}")
+                }
             } else {
                 i = mark
             }
