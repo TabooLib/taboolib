4f3a3fcd6a76acd70e7b24fb3a65ee6195bab9ab
坏黑 <b@skymc.me>
2022-08-16 14:33:43 +0800

    [6.0.9][Experimental-Update] Update ResettableLazy (recommend: 6.0.8-9)

--- common/src/main/kotlin/taboolib/common/util/LazyMaker.kt
+++ common/src/main/kotlin/taboolib/common/util/LazyMaker.kt
@@ -1,3 +1,94 @@
 package taboolib.common.util
 
+import java.util.LinkedList
+
 fun <T> unsafeLazy(initializer: () -> T): Lazy<T> = lazy(LazyThreadSafetyMode.NONE, initializer)
+
+fun <T> resettableLazy(group: String = "*", synchronized: Boolean = false, initializer: () -> T): ResettableLazy<T> {
+    return if (synchronized) {
+        ResettableSynchronizedLazyImpl(group, initializer)
+    } else {
+        ResettableLazyImpl(group, initializer)
+    }.also {
+        ResettableLazy.defined += it
+    }
+}
+
+abstract class ResettableLazy<T>(val group: String) : Lazy<T> {
+
+    abstract fun reset()
+
+    companion object {
+
+        val defined = LinkedList<ResettableLazy<*>>()
+
+        fun reset(group: String = "*") {
+            synchronized(defined) { defined.filter { it.group == group }.forEach { it.reset() } }
+        }
+    }
+}
+
+private class ResettableLazyImpl<T>(group: String, initializer: () -> T) : ResettableLazy<T>(group) {
+
+    private var initializer: (() -> T)? = initializer
+
+    private var localValue: Any? = UninitializedValue
+
+    @Suppress("UNCHECKED_CAST")
+    override val value: T
+        get() {
+            if (localValue === UninitializedValue) {
+                localValue = initializer!!()
+                initializer = null
+            }
+            return localValue as T
+        }
+
+    override fun reset() {
+        localValue = UninitializedValue
+    }
+
+    override fun isInitialized() = localValue !== UninitializedValue
+
+    override fun toString() = if (isInitialized()) value.toString() else "Lazy($group) value not initialized yet."
+}
+
+private class ResettableSynchronizedLazyImpl<T>(group: String, initializer: () -> T) : ResettableLazy<T>(group) {
+
+    private var initializer: (() -> T)? = initializer
+
+    @Volatile
+    private var localValue: Any? = UninitializedValue
+
+    private val lock = this
+
+    @Suppress("UNCHECKED_CAST")
+    override val value: T
+        get() {
+            val v1 = localValue
+            if (v1 !== UninitializedValue) {
+                return v1 as T
+            }
+            return synchronized(lock) {
+                val v2 = localValue
+                if (v2 !== UninitializedValue) {
+                    v2 as T
+                } else {
+                    val typedValue = initializer!!()
+                    localValue = typedValue
+                    initializer = null
+                    typedValue
+                }
+            }
+        }
+
+    override fun reset() {
+        localValue = UninitializedValue
+    }
+
+    override fun isInitialized() = localValue !== UninitializedValue
+
+    override fun toString() = if (isInitialized()) value.toString() else "Lazy($group) value not initialized yet."
+}
+
+internal object UninitializedValue
