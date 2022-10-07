2ef88dac7b4241f8edc616b42f9cdfcbb7155eeb
BuildTools <1349517404@qq.com>
2022-10-07 21:07:08 +0800

    + Bukkit Item序列化

--- expansion/expansion-geek-tool/src/main/kotlin/taboolib/expansion/geek/serialize/BukkitSerializer.kt
+++ expansion/expansion-geek-tool/src/main/kotlin/taboolib/expansion/geek/serialize/BukkitSerializer.kt
@@ -13,60 +13,76 @@ import java.io.IOException
  * 时间: 2022/7/28
  */
 
-    //反序列化 获取单个物品stack
-    fun String.deserializeItemStack(): ItemStack? {
-        ByteArrayInputStream(Base64Coder.decodeLines(this)).use {
-            BukkitObjectInputStream(it).use { var2 ->
-                return deserialize(var2.readObject())
+/**
+反序列化 获取单个物品stack
+ */
+fun String.deserializeItemStack(): ItemStack? {
+    ByteArrayInputStream(Base64Coder.decodeLines(this)).use {
+        BukkitObjectInputStream(it).use { var2 ->
+            return deserialize(var2.readObject())
+        }
+    }
+}
+/**
+ * 序列化 ItemStack 数组 为字符串
+ */
+fun ItemStack.serializeItemStacks(): String {
+    val byteOutputStream = ByteArrayOutputStream()
+    try {
+        BukkitObjectOutputStream(byteOutputStream).use {
+            it.writeObject(serialize(this))
+            return Base64Coder.encodeLines(byteOutputStream.toByteArray())
+        }
+    } catch (e: IOException) {
+        throw IllegalArgumentException("无法序列化物品堆栈数据")
+    }
+}
+
+/**
+ * 序列化 ItemStack 数组 为字符串
+ */
+fun Array<ItemStack>?.serializeItemStacks(): String {
+    if (this == null || this.isEmpty()) {
+        return ""
+    }
+    val byteOutputStream = ByteArrayOutputStream()
+    try {
+        BukkitObjectOutputStream(byteOutputStream).use {
+            it.writeInt(this.size)
+            for (items in this) {
+                it.writeObject(serialize(items))
             }
+            return Base64Coder.encodeLines(byteOutputStream.toByteArray())
         }
+    } catch (e: IOException) {
+        throw IllegalArgumentException("无法序列化物品堆栈数据")
     }
+}
 
-    /**
-     * 序列化 ItemStack 数组 为字符串
-     */
-    fun Array<ItemStack>?.serializeItemStacks(): String {
-        if (this == null || this.isEmpty()) {
-            return ""
-        }
-        val byteOutputStream = ByteArrayOutputStream()
-        try {
-            BukkitObjectOutputStream(byteOutputStream).use {
-                it.writeInt(this.size)
-                for (items in this) {
-                    it.writeObject(serialize(items))
-                }
-                return Base64Coder.encodeLines(byteOutputStream.toByteArray())
+/**
+ * 反序列化字符串为  ItemStack 数组
+ */
+fun String.deserializeItemStacks(): Array<ItemStack> {
+    if (this == "null" || this.isEmpty()) {
+        return emptyArray()
+    }
+    ByteArrayInputStream(Base64Coder.decodeLines(this)).use {
+        BukkitObjectInputStream(it).use { var2 ->
+            val contents = arrayOfNulls<ItemStack>(var2.readInt())
+            for ((index, _) in contents.withIndex()) {
+                contents[index] = deserialize(var2.readObject())
             }
-        } catch (e: IOException) {
-            throw IllegalArgumentException("无法序列化物品堆栈数据")
-        }
+            return contents.filterNotNull().toTypedArray()
+           }
     }
-
-    /**
-     * 反序列化字符串为  ItemStack 数组
-     */
-    fun String.deserializeItemStacks(): Array<ItemStack> {
-        if (this == "null" || this.isEmpty()) {
-            return emptyArray()
-        }
-        ByteArrayInputStream(Base64Coder.decodeLines(this)).use {
-            BukkitObjectInputStream(it).use { var2 ->
-                val contents = arrayOfNulls<ItemStack>(var2.readInt())
-                for ((index, _) in contents.withIndex()) {
-                    contents[index] = deserialize(var2.readObject())
-                }
-                return contents.filterNotNull().toTypedArray()
-            }
-        }
-    }
+}
 
 
 
-    private fun deserialize(item: Any?): ItemStack? {
-        return if (item != null) ItemStack.deserialize((item as Map<String, Any>)) else null
-    }
+private fun deserialize(item: Any?): ItemStack? {
+    return if (item != null) ItemStack.deserialize((item as Map<String, Any>)) else null
+}
 
-    private fun serialize(item: ItemStack?): Map<String, Any>? {
-        return item?.serialize()
-    }
+private fun serialize(item: ItemStack?): Map<String, Any>? {
+    return item?.serialize()
+}
