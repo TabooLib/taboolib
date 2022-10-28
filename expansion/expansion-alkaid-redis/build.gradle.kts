2a07bf1c4fcdcdd4a13dc17a844657a702ac3361
坏黑 <b@skymc.me>
2022-10-28 23:34:28 +0800

    [6.0.10] fix expansion-alkaid-redis

--- expansion/expansion-alkaid-redis/build.gradle.kts
+++ expansion/expansion-alkaid-redis/build.gradle.kts
@@ -10,6 +10,8 @@ tasks {
     withType<ShadowJar> {
         archiveClassifier.set("")
         relocate("redis.clients.jedis.", "redis.clients.jedis_4_2_3.")
+        relocate("com.electronwill.nightconfig.core.conversion", "taboolib.library.configuration")
+        relocate("org.yaml.snakeyaml.", "org.yaml.snakeyaml_1_32.")
         relocate("org.tabooproject", "taboolib.library")
     }
     build {
