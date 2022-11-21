73929dee7f25273adc89384ac1857ebabdcd26ff
坏黑 <b@skymc.me>
2022-11-21 10:04:00 +0800

    [6.0.10] update configuration

---
+++ module/module-configuration/src/main/kotlin/taboolib/module/configuration/util/Sections.kt
@@ -0,0 +1,25 @@
+package taboolib.module.configuration.util
+
+import taboolib.library.configuration.ConfigurationSection
+import taboolib.module.configuration.Configuration
+
+/**
+ * 将任意 [Map] 或 [Configuration] 转换为 [Map<String, Any?>]
+ */
+fun Any?.asMap(): Map<String, Any?> = when (this) {
+    is Map<*, *> -> entries.associate { it.key.toString() to it.value }
+    is ConfigurationSection -> getValues(false)
+    else -> emptyMap()
+}
+
+fun <V> ConfigurationSection.mapSection(transform: (ConfigurationSection) -> V): Map<String, V> {
+    return getKeys(false).associateWith { transform(getConfigurationSection(it)!!) }
+}
+
+fun <V> ConfigurationSection.mapSection(node: String, transform: (ConfigurationSection) -> V): Map<String, V> {
+    return getConfigurationSection(node)?.mapSection(transform) ?: emptyMap()
+}
+
+fun <T> ConfigurationSection.mapListAs(path: String, transform: (Map<String, Any?>) -> T): MutableList<T> {
+    return getMapList(path).map { transform(it.asMap()) }.toMutableList()
+}
