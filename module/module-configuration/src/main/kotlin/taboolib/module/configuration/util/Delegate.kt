f7f82e5ca24627827244e16c5e28d54070e6abf9
TONY_All <admin@minicraft.top>
2023-01-10 01:19:31 +0800

    optimize configuration delegate

--- module/module-configuration/src/main/kotlin/taboolib/module/configuration/util/Delegate.kt
+++ module/module-configuration/src/main/kotlin/taboolib/module/configuration/util/Delegate.kt
@@ -1,6 +1,7 @@
 package taboolib.module.configuration.util
 
 import taboolib.library.configuration.ConfigurationSection
+import taboolib.module.configuration.Configuration
 import taboolib.module.configuration.Configuration.Companion.getObject
 import taboolib.module.configuration.Configuration.Companion.setObject
 import kotlin.reflect.KProperty
@@ -13,21 +14,26 @@ import kotlin.reflect.KProperty
  * @author 寒雨
  * @since 2022/4/29 0:32
  **/
-inline operator fun <R, reified T> ConfigurationSection.getValue(thisRef: R, property: KProperty<*>): T {
-    val value = get(property.name)
-    if (value is ConfigurationSection) {
-        return getObject(property.name, true)
-    }
-    return get(property.name) as? T ?: error("wrong type for config delegate: ${property.name}")
-}
 
-inline operator fun <R, reified T : Any> ConfigurationSection.setValue(thisRef: R, property: KProperty<*>, value: T) {
-    when(value) {
-        is String, is Long,
-        is Short, is Int,
-        is Double, is Float,
-        is Boolean, is Collection<*>,
-        is ConfigurationSection, is Map<*, *> -> set(property.name, value)
-        else -> setObject(property.name, value)
+fun config(config: Configuration, path: String? = null) = Delegate(config, path)
+
+class Delegate(val config: Configuration, val path: String?) {
+    inline operator fun <R, reified T> getValue(thisRef: R, property: KProperty<*>): T {
+        val value = config[property.name]
+        if (value is ConfigurationSection) {
+            return config.getObject(property.name, true)
+        }
+        return  config[path ?: property.name] as? T ?: error("wrong type for config delegate: ${path ?: property.name}")
+    }
+
+    inline operator fun <R, reified T : Any> setValue(thisRef: R, property: KProperty<*>, value: T) {
+        when(value) {
+            is String, is Long,
+            is Short, is Int,
+            is Double, is Float,
+            is Boolean, is Collection<*>,
+            is ConfigurationSection, is Map<*, *> -> config[path ?: property.name] = value
+            else -> config.setObject(path ?: property.name, value)
+        }
     }
 }
