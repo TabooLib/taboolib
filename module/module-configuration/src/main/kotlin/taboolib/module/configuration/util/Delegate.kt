18a33b79ac69bdb8e43dd851062d37f9c0b686fe
TONY_All <admin@minicraft.top>
2023-01-10 14:20:01 +0800

    fix delegate

--- module/module-configuration/src/main/kotlin/taboolib/module/configuration/util/Delegate.kt
+++ module/module-configuration/src/main/kotlin/taboolib/module/configuration/util/Delegate.kt
@@ -5,6 +5,7 @@ import taboolib.module.configuration.Configuration
 import taboolib.module.configuration.Configuration.Companion.getObject
 import taboolib.module.configuration.Configuration.Companion.setObject
 import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty0
 
 /**
  * taboolib.module.configuration.util.Delegate
@@ -15,25 +16,26 @@ import kotlin.reflect.KProperty
  * @since 2022/4/29 0:32
  **/
 
-fun config(config: Configuration, path: String? = null) = Delegate(config, path)
+fun config(config: KProperty0<Configuration>, path: String? = null) = Delegate(config , path)
 
-class Delegate(val config: Configuration, val path: String?) {
+class Delegate(val config: KProperty0<Configuration>, val path: String?) {
     inline operator fun <R, reified T> getValue(thisRef: R, property: KProperty<*>): T {
-        val value = config[property.name]
+        val value = config.get()[path ?: property.name]
         if (value is ConfigurationSection) {
-            return config.getObject(property.name, true)
+            return config.get().getObject(path ?: property.name, true)
         }
-        return  config[path ?: property.name] as? T ?: error("wrong type for config delegate: ${path ?: property.name}")
+        return value as? T ?: error("wrong type for config delegate: ${path ?: property.name}")
     }
 
     inline operator fun <R, reified T : Any> setValue(thisRef: R, property: KProperty<*>, value: T) {
-        when(value) {
+        when (value) {
             is String, is Long,
             is Short, is Int,
             is Double, is Float,
             is Boolean, is Collection<*>,
-            is ConfigurationSection, is Map<*, *> -> config[path ?: property.name] = value
-            else -> config.setObject(path ?: property.name, value)
+            is ConfigurationSection, is Map<*, *> -> config.get()[path ?: property.name] = value
+
+            else -> config.get().setObject(path ?: property.name, value)
         }
     }
 }
