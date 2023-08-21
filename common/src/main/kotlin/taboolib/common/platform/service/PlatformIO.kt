f45a84d55083a2cf8ea7c6bee2c7a1a59d1b1052
MC~蛟龙 <1610105206@qq.com>
2023-08-21 21:42:33 +0800

    修复一个小错误 (我是蠢比)

--- common/src/main/kotlin/taboolib/common/platform/service/PlatformIO.kt
+++ common/src/main/kotlin/taboolib/common/platform/service/PlatformIO.kt
@@ -20,7 +20,7 @@ interface PlatformIO {
 
     fun warning(vararg message: Any?)
 
-    fun releaseResourceFile(source: String, target: String = resource, replace: Boolean = false): File
+    fun releaseResourceFile(source: String, target: String = source, replace: Boolean = false): File
 
     fun getJarFile(): File
 
