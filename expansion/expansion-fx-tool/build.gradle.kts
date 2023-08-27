a883112b07869bbd6305b5555a6fadd473a9db00
FxRayHughes <bushuaichen@yeah.net>
2023-08-27 17:47:48 +0800

    新增一个例子

---
+++ expansion/expansion-fx-tool/build.gradle.kts
@@ -0,0 +1,11 @@
+dependencies {
+    compileOnly(project(":common"))
+    compileOnly(project(":common-5"))
+    compileOnly(project(":module:module-nms-util"))
+    compileOnly(project(":module:module-nms"))
+    compileOnly(project(":module:module-chat"))
+    compileOnly(project(":platform:platform-bukkit"))
+    compileOnly("ink.ptms.core:v12001:12001:mapped")
+    compileOnly("ink.ptms.core:v12001:12001:universal")
+    compileOnly("ink.ptms:nms-all:1.0.0")
+}
