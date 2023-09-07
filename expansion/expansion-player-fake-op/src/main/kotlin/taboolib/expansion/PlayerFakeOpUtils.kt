f6cf5ac82ccf527f230d0197340438cce24a9e39
Sunshine-wzy <sunshinewzy@qq.com>
2023-09-07 18:44:54 +0800

    Add Player.dispatchCommandAsOp function

--- expansion/expansion-player-fake-op/src/main/kotlin/taboolib/expansion/PlayerFakeOpUtils.kt
+++ expansion/expansion-player-fake-op/src/main/kotlin/taboolib/expansion/PlayerFakeOpUtils.kt
@@ -1,5 +1,6 @@
 package taboolib.expansion
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 /**
@@ -11,3 +12,9 @@ import org.bukkit.entity.Player
  * @return [this] 的 PlayerFakeOp 代理对象
  */
 fun Player.fakeOp(): Player = PlayerFakeOpNMS.INSTANCE.playerFakeOp(this)
+
+/**
+ * 以OP的权限执行指令 (仅针对非原版指令)
+ */
+fun Player.dispatchCommandAsOp(command: String): Boolean =
+    Bukkit.dispatchCommand(fakeOp(), command)
