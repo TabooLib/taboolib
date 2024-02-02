9fd7dac598bcb98552aa60fa3bf566370e6ba7d1
BingZi-233 <lhby233@outlook.com>
2024-02-02 09:53:54 +0800

    [6.1.0][dev] 移除不再受到维护的expansion

--- expansion/expansion-lang-tools/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    compileOnly(project(":common"))
-    compileOnly(project(":common-platform-api"))
-    compileOnly(project(":module:module-chat"))
-    compileOnly(project(":module:module-lang"))
-    compileOnly(project(":platform:platform-bukkit"))
-    compileOnly("ink.ptms.core:v11701:11701-minimize:universal")
-}
