65654d71a1f6ef8241eb2c0a5b69cfe83bf59a90
Bkm016 <sky@asgard.me>
2024-03-15 14:21:01 +0800

    [6.1.0][dev] 调整 NMSItemTag

--- module/module-nms-util/src/main/kotlin/taboolib/module/nms/NMSItemTag.kt
+++ module/module-nms-util/src/main/kotlin/taboolib/module/nms/NMSItemTag.kt
@@ -57,28 +57,28 @@ abstract class NMSItemTag {
 @Suppress("SpellCheckingInspection", "UNCHECKED_CAST")
 class NMSItemTagImpl : NMSItemTag() {
 
-    val nbtTagCompoundGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagCompound>(if (MinecraftVersion.isUniversal) "x" else "map")
-    val nbtTagListGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagList>(if (MinecraftVersion.isUniversal) "c" else "list")
-    val nbtTagListTypeSetter = unreflectSetter<net.minecraft.server.v1_12_R1.NBTTagList>(if (MinecraftVersion.isUniversal) "w" else "type")
-    val nbtTagByteGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagByte>(if (MinecraftVersion.isUniversal) "x" else "data")
-    val nbtTagShortGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagShort>(if (MinecraftVersion.isUniversal) "c" else "data")
-    val nbtTagIntGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagInt>(if (MinecraftVersion.isUniversal) "c" else "data")
-    val nbtTagLongGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagLong>(if (MinecraftVersion.isUniversal) "c" else "data")
-    val nbtTagFloatGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagFloat>(if (MinecraftVersion.isUniversal) "w" else "data")
-    val nbtTagDoubleGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagDouble>(if (MinecraftVersion.isUniversal) "w" else "data")
-    val nbtTagStringGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagString>(if (MinecraftVersion.isUniversal) "A" else "data")
-    val nbtTagByteArrayGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagByteArray>(if (MinecraftVersion.isUniversal) "c" else "data")
-    val nbtTagIntArrayGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagIntArray>(if (MinecraftVersion.isUniversal) "c" else "data")
-    val nbtTagLongArrayGetter = unreflectGetter<net.minecraft.server.v1_12_R1.NBTTagLongArray>(if (MinecraftVersion.isUniversal) "c" else "b")
+    val nbtTagCompoundGetter = unreflectGetter<NBTTagCompound12>(if (MinecraftVersion.isUniversal) "x" else "map")
+    val nbtTagListGetter = unreflectGetter<NBTTagList12>(if (MinecraftVersion.isUniversal) "c" else "list")
+    val nbtTagListTypeSetter = unreflectSetter<NBTTagList12>(if (MinecraftVersion.isUniversal) "w" else "type")
+    val nbtTagByteGetter = unreflectGetter<NBTTagByte12>(if (MinecraftVersion.isUniversal) "x" else "data")
+    val nbtTagShortGetter = unreflectGetter<NBTTagShort12>(if (MinecraftVersion.isUniversal) "c" else "data")
+    val nbtTagIntGetter = unreflectGetter<NBTTagInt12>(if (MinecraftVersion.isUniversal) "c" else "data")
+    val nbtTagLongGetter = unreflectGetter<NBTTagLong12>(if (MinecraftVersion.isUniversal) "c" else "data")
+    val nbtTagFloatGetter = unreflectGetter<NBTTagFloat12>(if (MinecraftVersion.isUniversal) "w" else "data")
+    val nbtTagDoubleGetter = unreflectGetter<NBTTagDouble12>(if (MinecraftVersion.isUniversal) "w" else "data")
+    val nbtTagStringGetter = unreflectGetter<NBTTagString12>(if (MinecraftVersion.isUniversal) "A" else "data")
+    val nbtTagByteArrayGetter = unreflectGetter<NBTTagByteArray12>(if (MinecraftVersion.isUniversal) "c" else "data")
+    val nbtTagIntArrayGetter = unreflectGetter<NBTTagIntArray12>(if (MinecraftVersion.isUniversal) "c" else "data")
+    val nbtTagLongArrayGetter = unreflectGetter<NBTTagLongArray12>(if (MinecraftVersion.isUniversal) "c" else "b")
 
     override fun getItemTag(itemStack: ItemStack): ItemTag {
-        val nmsItem = NMSItem.asNMSCopy(itemStack) as net.minecraft.server.v1_12_R1.ItemStack
+        val nmsItem = NMSItem.asNMSCopy(itemStack) as NMSItemStack12
         return if (nmsItem.hasTag()) itemTagToBukkitCopy(nmsItem.tag!!).asCompound() else ItemTag()
     }
 
     override fun setItemTag(itemStack: ItemStack, itemTag: ItemTag): ItemStack {
-        val nmsItem = NMSItem.asNMSCopy(itemStack) as net.minecraft.server.v1_12_R1.ItemStack
-        nmsItem.tag = itemTagToNMSCopy(itemTag) as net.minecraft.server.v1_12_R1.NBTTagCompound
+        val nmsItem = NMSItem.asNMSCopy(itemStack) as NMSItemStack12
+        nmsItem.tag = itemTagToNMSCopy(itemTag) as NBTTagCompound12
         return NMSItem.asBukkitCopy(nmsItem)
     }
 
@@ -90,22 +90,22 @@ class NMSItemTagImpl : NMSItemTag() {
         val new = MinecraftVersion.isHigherOrEqual(MinecraftVersion.V1_15)
         return when (itemTagData.type) {
             // 基本类型
-            ItemTagType.BYTE -> if (new) net.minecraft.server.v1_15_R1.NBTTagByte.a(itemTagData.asByte()) else net.minecraft.server.v1_12_R1.NBTTagByte(itemTagData.asByte())
-            ItemTagType.SHORT -> if (new) net.minecraft.server.v1_15_R1.NBTTagShort.a(itemTagData.asShort()) else net.minecraft.server.v1_12_R1.NBTTagShort(itemTagData.asShort())
-            ItemTagType.INT -> if (new) net.minecraft.server.v1_15_R1.NBTTagInt.a(itemTagData.asInt()) else net.minecraft.server.v1_12_R1.NBTTagInt(itemTagData.asInt())
-            ItemTagType.LONG -> if (new) net.minecraft.server.v1_15_R1.NBTTagLong.a(itemTagData.asLong()) else net.minecraft.server.v1_12_R1.NBTTagLong(itemTagData.asLong())
-            ItemTagType.FLOAT -> if (new) net.minecraft.server.v1_15_R1.NBTTagFloat.a(itemTagData.asFloat()) else net.minecraft.server.v1_12_R1.NBTTagFloat(itemTagData.asFloat())
-            ItemTagType.DOUBLE -> if (new) net.minecraft.server.v1_15_R1.NBTTagDouble.a(itemTagData.asDouble()) else net.minecraft.server.v1_12_R1.NBTTagDouble(itemTagData.asDouble())
-            ItemTagType.STRING -> if (new) net.minecraft.server.v1_15_R1.NBTTagString.a(itemTagData.asString()) else net.minecraft.server.v1_12_R1.NBTTagString(itemTagData.asString())
+            ItemTagType.BYTE -> if (new) NBTTagByte15.a(itemTagData.asByte()) else NBTTagByte12(itemTagData.asByte())
+            ItemTagType.SHORT -> if (new) NBTTagShort15.a(itemTagData.asShort()) else NBTTagShort12(itemTagData.asShort())
+            ItemTagType.INT -> if (new) NBTTagInt15.a(itemTagData.asInt()) else NBTTagInt12(itemTagData.asInt())
+            ItemTagType.LONG -> if (new) NBTTagLong15.a(itemTagData.asLong()) else NBTTagLong12(itemTagData.asLong())
+            ItemTagType.FLOAT -> if (new) NBTTagFloat15.a(itemTagData.asFloat()) else NBTTagFloat12(itemTagData.asFloat())
+            ItemTagType.DOUBLE -> if (new) NBTTagDouble15.a(itemTagData.asDouble()) else NBTTagDouble12(itemTagData.asDouble())
+            ItemTagType.STRING -> if (new) NBTTagString15.a(itemTagData.asString()) else NBTTagString12(itemTagData.asString())
 
             // 数组类型特殊处理
-            ItemTagType.BYTE_ARRAY -> net.minecraft.server.v1_12_R1.NBTTagByteArray(itemTagData.asByteArray().copyOf())
-            ItemTagType.INT_ARRAY -> net.minecraft.server.v1_12_R1.NBTTagIntArray(itemTagData.asIntArray().copyOf())
-            ItemTagType.LONG_ARRAY -> net.minecraft.server.v1_12_R1.NBTTagLongArray(itemTagData.asLongArray().copyOf())
+            ItemTagType.BYTE_ARRAY -> NBTTagByteArray12(itemTagData.asByteArray().copyOf())
+            ItemTagType.INT_ARRAY -> NBTTagIntArray12(itemTagData.asIntArray().copyOf())
+            ItemTagType.LONG_ARRAY -> NBTTagLongArray12(itemTagData.asLongArray().copyOf())
 
             // 列表类型特殊处理
             ItemTagType.LIST -> {
-                net.minecraft.server.v1_12_R1.NBTTagList().also { nmsList ->
+                NBTTagList12().also { nmsList ->
                     // 反射获取字段：
                     // private final List<NBTBase> list;
                     val list = nbtTagListGetter.get<MutableList<Any>>(nmsList)
@@ -120,7 +120,7 @@ class NMSItemTagImpl : NMSItemTag() {
 
             // 复合类型特殊处理
             ItemTagType.COMPOUND -> {
-                net.minecraft.server.v1_12_R1.NBTTagCompound().also { nmsCompound ->
+                NBTTagCompound12().also { nmsCompound ->
                     // 反射获取字段：
                     // private final Map<String, NBTBase> map
                     val map = nbtTagCompoundGetter.get<MutableMap<String, Any>>(nmsCompound)
@@ -136,26 +136,26 @@ class NMSItemTagImpl : NMSItemTag() {
     override fun itemTagToBukkitCopy(nbtTag: Any): ItemTagData {
         return when (nbtTag) {
             // 基本类型
-            is net.minecraft.server.v1_12_R1.NBTTagByte -> ItemTagData(ItemTagType.BYTE, nbtTagByteGetter.get(nbtTag))
-            is net.minecraft.server.v1_12_R1.NBTTagShort -> ItemTagData(ItemTagType.SHORT, nbtTagShortGetter.get(nbtTag))
-            is net.minecraft.server.v1_12_R1.NBTTagInt -> ItemTagData(ItemTagType.INT, nbtTagIntGetter.get(nbtTag))
-            is net.minecraft.server.v1_12_R1.NBTTagLong -> ItemTagData(ItemTagType.LONG, nbtTagLongGetter.get(nbtTag))
-            is net.minecraft.server.v1_12_R1.NBTTagFloat -> ItemTagData(ItemTagType.FLOAT, nbtTagFloatGetter.get(nbtTag))
-            is net.minecraft.server.v1_12_R1.NBTTagDouble -> ItemTagData(ItemTagType.DOUBLE, nbtTagDoubleGetter.get(nbtTag))
-            is net.minecraft.server.v1_12_R1.NBTTagString -> ItemTagData(ItemTagType.STRING, nbtTagStringGetter.get(nbtTag))
+            is NBTTagByte12 -> ItemTagData(ItemTagType.BYTE, nbtTagByteGetter.get(nbtTag))
+            is NBTTagShort12 -> ItemTagData(ItemTagType.SHORT, nbtTagShortGetter.get(nbtTag))
+            is NBTTagInt12 -> ItemTagData(ItemTagType.INT, nbtTagIntGetter.get(nbtTag))
+            is NBTTagLong12 -> ItemTagData(ItemTagType.LONG, nbtTagLongGetter.get(nbtTag))
+            is NBTTagFloat12 -> ItemTagData(ItemTagType.FLOAT, nbtTagFloatGetter.get(nbtTag))
+            is NBTTagDouble12 -> ItemTagData(ItemTagType.DOUBLE, nbtTagDoubleGetter.get(nbtTag))
+            is NBTTagString12 -> ItemTagData(ItemTagType.STRING, nbtTagStringGetter.get(nbtTag))
 
             // 数组类型特殊处理
-            is net.minecraft.server.v1_12_R1.NBTTagByteArray -> ItemTagData(ItemTagType.BYTE_ARRAY, nbtTagByteArrayGetter.get<ByteArray>(nbtTag).copyOf())
-            is net.minecraft.server.v1_12_R1.NBTTagIntArray -> ItemTagData(ItemTagType.INT_ARRAY, nbtTagIntArrayGetter.get<IntArray>(nbtTag).copyOf())
-            is net.minecraft.server.v1_12_R1.NBTTagLongArray -> ItemTagData(ItemTagType.LONG_ARRAY, nbtTagLongArrayGetter.get<LongArray>(nbtTag).copyOf())
+            is NBTTagByteArray12 -> ItemTagData(ItemTagType.BYTE_ARRAY, nbtTagByteArrayGetter.get<ByteArray>(nbtTag).copyOf())
+            is NBTTagIntArray12 -> ItemTagData(ItemTagType.INT_ARRAY, nbtTagIntArrayGetter.get<IntArray>(nbtTag).copyOf())
+            is NBTTagLongArray12 -> ItemTagData(ItemTagType.LONG_ARRAY, nbtTagLongArrayGetter.get<LongArray>(nbtTag).copyOf())
 
             // 列表类型特殊处理
-            is net.minecraft.server.v1_12_R1.NBTTagList -> {
+            is NBTTagList12 -> {
                 ItemTagList(nbtTagListGetter.get<List<Any>>(nbtTag).map { itemTagToBukkitCopy(it) })
             }
 
             // 复合类型特殊处理
-            is net.minecraft.server.v1_12_R1.NBTTagCompound -> {
+            is NBTTagCompound12 -> {
                 ItemTag().apply { nbtTagCompoundGetter.get<Map<String, Any>>(nbtTag).forEach { put(it.key, itemTagToBukkitCopy(it.value)) } }
             }
 
@@ -191,3 +191,31 @@ private fun ItemStack?.validation(): ItemStack {
         return this
     }
 }
+
+typealias NMSItemStack12 = net.minecraft.server.v1_12_R1.ItemStack
+typealias NBTTagCompound12 = net.minecraft.server.v1_12_R1.NBTTagCompound
+typealias NBTTagList12 = net.minecraft.server.v1_12_R1.NBTTagList
+typealias NBTTagByte12 = net.minecraft.server.v1_12_R1.NBTTagByte
+typealias NBTTagShort12 = net.minecraft.server.v1_12_R1.NBTTagShort
+typealias NBTTagInt12 = net.minecraft.server.v1_12_R1.NBTTagInt
+typealias NBTTagLong12 = net.minecraft.server.v1_12_R1.NBTTagLong
+typealias NBTTagFloat12 = net.minecraft.server.v1_12_R1.NBTTagFloat
+typealias NBTTagDouble12 = net.minecraft.server.v1_12_R1.NBTTagDouble
+typealias NBTTagString12 = net.minecraft.server.v1_12_R1.NBTTagString
+typealias NBTTagByteArray12 = net.minecraft.server.v1_12_R1.NBTTagByteArray
+typealias NBTTagIntArray12 = net.minecraft.server.v1_12_R1.NBTTagIntArray
+typealias NBTTagLongArray12 = net.minecraft.server.v1_12_R1.NBTTagLongArray
+
+typealias NMSItemStack15 = net.minecraft.server.v1_15_R1.ItemStack
+typealias NBTTagCompound15 = net.minecraft.server.v1_15_R1.NBTTagCompound
+typealias NBTTagList15 = net.minecraft.server.v1_15_R1.NBTTagList
+typealias NBTTagByte15 = net.minecraft.server.v1_15_R1.NBTTagByte
+typealias NBTTagShort15 = net.minecraft.server.v1_15_R1.NBTTagShort
+typealias NBTTagInt15 = net.minecraft.server.v1_15_R1.NBTTagInt
+typealias NBTTagLong15 = net.minecraft.server.v1_15_R1.NBTTagLong
+typealias NBTTagFloat15 = net.minecraft.server.v1_15_R1.NBTTagFloat
+typealias NBTTagDouble15 = net.minecraft.server.v1_15_R1.NBTTagDouble
+typealias NBTTagString15 = net.minecraft.server.v1_15_R1.NBTTagString
+typealias NBTTagByteArray15 = net.minecraft.server.v1_15_R1.NBTTagByteArray
+typealias NBTTagIntArray15 = net.minecraft.server.v1_15_R1.NBTTagIntArray
+typealias NBTTagLongArray15 = net.minecraft.server.v1_15_R1.NBTTagLongArray
