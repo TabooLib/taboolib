a6dec4dbfb11a769dc3ab32bd3b9cee88f182e6e
Ling_Haner <ling_haner@163.com>
2024-03-30 19:14:19 +0800

    依赖修改

--- platform/platform-afybroker/build.gradle.kts
+++ platform/platform-afybroker/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     compileOnly(project(":common"))
     compileOnly(project(":common-platform-api"))
-    compileOnly(fileTree("libs"))
+    compileOnly("net.afyer:afybroker-server:1.5")
 }
