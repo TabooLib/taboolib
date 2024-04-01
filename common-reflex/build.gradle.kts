13ab67a8924ba4c39312065a30c20120e2c1b1dd
Bkm016 <sky@asgard.me>
2024-04-01 22:46:11 +0800

    [6.1.0][dev] 更新 Reflex 版本

--- common-reflex/build.gradle.kts
+++ common-reflex/build.gradle.kts
@@ -1,15 +1,15 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 dependencies {
-    implementation("org.tabooproject.reflex:reflex:1.0.22")
-    implementation("org.tabooproject.reflex:analyser:1.0.22")
+    implementation("org.tabooproject.reflex:reflex:1.0.23")
+    implementation("org.tabooproject.reflex:analyser:1.0.23")
 }
 
 tasks {
     withType<ShadowJar> {
         dependencies {
-            include(dependency("org.tabooproject.reflex:reflex:1.0.22"))
-            include(dependency("org.tabooproject.reflex:analyser:1.0.22"))
+            include(dependency("org.tabooproject.reflex:reflex:1.0.23"))
+            include(dependency("org.tabooproject.reflex:analyser:1.0.23"))
         }
         relocate("org.taboooproject", "taboolib.library")
     }
