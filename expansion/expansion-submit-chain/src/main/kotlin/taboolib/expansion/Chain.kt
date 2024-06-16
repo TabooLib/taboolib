cff9d0afd962eac39c18e48604373bdcd310cb1f
kirraObj <c@skymc.me>
2024-06-16 14:50:24 +0800

    update

--- expansion/expansion-submit-chain/src/main/kotlin/taboolib/expansion/Chain.kt
+++ expansion/expansion-submit-chain/src/main/kotlin/taboolib/expansion/Chain.kt
@@ -2,28 +2,42 @@ package taboolib.expansion
 
 import kotlinx.coroutines.*
 import taboolib.common.platform.function.submit
+import taboolib.expansion.DispatcherType.ASYNC
+import taboolib.expansion.DispatcherType.SYNC
+import taboolib.expansion.DurationType.MILLIS
+import taboolib.expansion.DurationType.MINECRAFT_TICK
+import java.util.concurrent.CompletableFuture
 import kotlin.coroutines.CoroutineContext
+
+enum class DispatcherType {
+
+    SYNC, ASYNC
+}
+
+enum class DurationType {
+
+    MINECRAFT_TICK, MILLIS
+}
 
 object SyncDispatcher : CoroutineDispatcher() {
 
     override fun dispatch(context: CoroutineContext, block: Runnable) {
-        submit { block.run() }
+        submit(now = true) { block.run() }
     }
 }
 
 object AsyncDispatcher : CoroutineDispatcher() {
 
     override fun dispatch(context: CoroutineContext, block: Runnable) {
-        submit(async = true) { block.run() }
+        submit(now = true, async = true) { block.run() }
     }
 }
 
-open class Chain(val chain: suspend Chain.() -> Unit) {
+open class Chain<R>(val chain: suspend Chain<R>.() -> R) {
 
-    suspend fun wait(value: Long) {
-        withContext(AsyncDispatcher) {
-            // 50ms = 1 tick in Minecraft
-            delay(value * 50)
+    suspend fun <T> async(block: () -> T): T {
+        return withContext(AsyncDispatcher) {
+            block()
         }
     }
 
@@ -33,41 +47,38 @@ open class Chain(val chain: suspend Chain.() -> Unit) {
         }
     }
 
-    suspend fun <T> async(block: () -> T): T {
-        return withContext(AsyncDispatcher) {
-            block()
+    suspend fun wait(value: Long, type: DurationType) {
+        withContext(AsyncDispatcher) {
+            when (type) {
+                // 50ms = 1 tick in Minecraft
+                MINECRAFT_TICK -> delay(value * 50)
+                MILLIS -> value
+            }
         }
     }
 
-    suspend fun <T> sync(period: Long, delay: Long = 0L, block: Cancellable.() -> T): T {
+    suspend fun <T> sync(period: Long, now: Boolean = false, delay: Long = 0L, block: Cancellable.() -> T): T {
         return withContext(SyncDispatcher) {
-            SynchronousRepeatChain(block, period, delay).execute()
+            SynchronousRepeatChain(block, period, now, delay).execute()
         }
     }
 
-    suspend fun <T> async(period: Long, delay: Long = 0L, block: Cancellable.() -> T): T {
+    suspend fun <T> async(period: Long, now: Boolean = false, delay: Long = 0L, block: Cancellable.() -> T): T {
         return withContext(AsyncDispatcher) {
-            AsynchronousRepeatChain(block, period, delay).execute()
+            AsynchronousRepeatChain(block, period, now, delay).execute()
         }
     }
 
-//    suspend fun sync(period: Long, delay: Long = 0L, block: Cancellable.() -> Unit) {
-//        withContext(SyncDispatcher) {
-//            SynchronousRepeatChain(block, period, delay).execute()
-//        }
-//    }
-//
-//    suspend fun async(period: Long, delay: Long = 0L, block: Cancellable.() -> Unit) {
-//        withContext(AsyncDispatcher) {
-//            AsynchronousRepeatChain(block, period, delay).execute()
-//        }
-//    }
-
-    fun run() {
-        CoroutineScope(AsyncDispatcher).launch { chain(this@Chain) }
+    fun run(type: DispatcherType): CompletableFuture<R> {
+        val future = CompletableFuture<R>()
+        when (type) {
+            SYNC -> CoroutineScope(SyncDispatcher).launch { future.complete(chain(this@Chain)) }
+            ASYNC -> CoroutineScope(AsyncDispatcher).launch { future.complete(chain(this@Chain)) }
+        }
+        return future
     }
 }
 
-fun submitChain(block: suspend Chain.() -> Unit) {
-    Chain(block).run()
+fun <R> submitChain(type: DispatcherType = ASYNC, block: suspend Chain<R>.() -> R): CompletableFuture<R> {
+    return Chain(block).run(type)
 }
