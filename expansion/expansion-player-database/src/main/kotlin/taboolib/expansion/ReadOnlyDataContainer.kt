cfa880c76377151da1ff72ea9048159133338bf9
FxRayHughes <bushuaichen@yeah.net>
2024-08-01 15:03:08 +0800

    [player-database] + 增加了一个同步功能，只读类型容器会周期同步数据库的内容

--- expansion/expansion-player-database/src/main/kotlin/taboolib/expansion/ReadOnlyDataContainer.kt
+++ expansion/expansion-player-database/src/main/kotlin/taboolib/expansion/ReadOnlyDataContainer.kt
@@ -1,9 +1,14 @@
 package taboolib.expansion
 
+import taboolib.common.Inject
+import taboolib.common.LifeCycle
+import taboolib.common.platform.Awake
+import taboolib.common.platform.function.submitAsync
+
 // 只读数据容器
 class ReadOnlyDataContainer(val user: String, val database: Database) {
 
-    val source = database[user]
+    var source = database[user]
 
     operator fun get(key: String): String? {
         return source[key]
@@ -23,5 +28,26 @@ class ReadOnlyDataContainer(val user: String, val database: Database) {
 
     override fun toString(): String {
         return "ReadOnlyDataContainer(user='$user', source=$source)"
+    }
+
+    fun update() {
+        source = database[user]
+    }
+
+
+    @Inject
+    internal companion object {
+
+        // 延迟更新时间
+        var syncTick = 80L
+
+        @Awake(LifeCycle.ACTIVE)
+        private fun update() {
+            submitAsync(period = syncTick) {
+                playerReadOnlyDataContainer.values.forEach {
+                    runCatching { it.update() }
+                }
+            }
+        }
     }
 }
