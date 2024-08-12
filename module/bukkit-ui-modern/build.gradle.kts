2bff5e9631bafd5be78fdbca6e4a04470211c481
Micalhl <micalhl@sina.cn>
2024-08-12 16:06:53 +0800

    [6.2.0][dev] Optimize

--- module/bukkit-ui-modern/build.gradle.kts
+++ module/bukkit-ui-modern/build.gradle.kts
@@ -1,12 +1,5 @@
-repositories {
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-}
-
 dependencies {
-    // 这是假的 1.21 吧, 我服了
-    // 明明 1.21 里 InventoryView 是个 interface, 这里怎么不是??
-    // compileOnly("ink.ptms.core:v12100:12100:mapped")
-    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("ink.ptms.core:v12101:12101:mapped")
 }
 
 // 推送第一次会失败, 第二次就成功了, 不知为何
