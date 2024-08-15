88664fdcccee52632f3b9d00d961db1107e72c49
MufHead <124489362@qq.com>
2024-08-15 18:31:37 +0800

    SingleRedisConnection注释

--- expansion/database-alkaid-redis/src/main/kotlin/taboolib/expansion/SingleRedisConnection.kt
+++ expansion/database-alkaid-redis/src/main/kotlin/taboolib/expansion/SingleRedisConnection.kt
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2022 Alkaid
  *
@@ -67,11 +68,20 @@ class SingleRedisConnection(internal var pool: JedisPool, internal val connector
         }
     }
 
+    /**
+     * 关闭连接
+     */
     override fun close() {
         pool.destroy()
     }
 
-    override fun set(key: String, value: String?) {
+    /**
+     * 赋值
+     *
+     * @param key 键
+     * @param value 值
+     */
+    override operator fun set(key: String, value: String?) {
         exec { if (value == null) it.del(key) else it[key] = value }
     }
 
@@ -79,22 +89,51 @@ class SingleRedisConnection(internal var pool: JedisPool, internal val connector
         exec { if (value == null) it.del(key) else it.setnx(key, value) }
     }
 
-    override fun get(key: String): String? {
+    /**
+     * 取值
+     *
+     * @param key 键
+     * @return 值
+     */
+    override operator fun get(key: String): String? {
         return exec { it[key] }
     }
 
+    /**
+     * 删除
+     *
+     * @param key 键
+     */
     override fun delete(key: String) {
         exec { it.del(key) }
     }
 
+    /**
+     * 赋值并设置过期时间
+     *
+     * @param key 键
+     * @param value 值
+     */
     override fun expire(key: String, value: Long, timeUnit: TimeUnit) {
         exec { it.expire(key, timeUnit.toSeconds(value)) }
     }
 
+    /**
+     * 是否存在
+     *
+     * @param key
+     * @return Boolean
+     */
     override fun contains(key: String): Boolean {
         return exec { it.exists(key) }
     }
 
+    /**
+     * 推送信息
+     *
+     * @param channel 频道
+     * @param message 消息
+     */
     override fun publish(channel: String, message: Any) {
         exec {
             if (message is String) {
@@ -105,6 +144,13 @@ class SingleRedisConnection(internal var pool: JedisPool, internal val connector
         }
     }
 
+    /**
+     * 订阅频道
+     *
+     * @param channel 频道
+     * @param patternMode 频道名称是否为正则模式
+     * @param func 信息处理函数
+     */
     override fun subscribe(vararg channel: String, patternMode: Boolean, func: RedisMessage.() -> Unit) {
         service.submit {
             try {
@@ -175,4 +221,3 @@ class SingleRedisConnection(internal var pool: JedisPool, internal val connector
         }
     }
 }
-
