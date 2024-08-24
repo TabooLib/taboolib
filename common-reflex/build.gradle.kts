6ff842f83f986f221cdd38108eca4cf029b5d123
黑 <b@taboo.sh>
2024-08-25 03:50:35 +0800

    更新 Reflex 版本为 1.1.0

--- common-reflex/build.gradle.kts
+++ common-reflex/build.gradle.kts
@@ -1,15 +1,15 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 dependencies {
-    implementation("org.tabooproject.reflex:reflex:1.0.25-test-11")
-    implementation("org.tabooproject.reflex:analyser:1.0.25-test-11")
+    implementation("org.tabooproject.reflex:reflex:1.1.0")
+    implementation("org.tabooproject.reflex:analyser:1.1.0")
 }
 
 tasks {
     withType<ShadowJar> {
         dependencies {
-            include(dependency("org.tabooproject.reflex:reflex:1.0.25-test-11"))
-            include(dependency("org.tabooproject.reflex:analyser:1.0.25-test-11"))
+            include(dependency("org.tabooproject.reflex:reflex:1.1.0"))
+            include(dependency("org.tabooproject.reflex:analyser:1.1.0"))
         }
         relocate("org.taboooproject", "taboolib.library")
     }
