c55ef334cff18af4a0bb1a719d4873b833c747fa
FxRayHughes <bushuaichen@yeah.net>
2024-09-21 10:08:59 +0800

    [Add] 新增了Redis-PlayerDatabase

---
+++ module/database/database-player-redis/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    compileOnly(project(":common"))
+    compileOnly(project(":common-util"))
+    compileOnly(project(":common-platform-api"))
+    compileOnly(project(":module:database"))
+    compileOnly(project(":module:database:database-player"))
+    compileOnly(project(":module:database:database-alkaid-redis"))
+    compileOnly(project(":module:basic:basic-configuration"))
+    compileOnly("ink.ptms.core:v11701:11701-minimize:universal")
+}
