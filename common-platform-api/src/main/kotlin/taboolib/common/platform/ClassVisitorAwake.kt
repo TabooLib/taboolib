d2bd6c43a8ec842075a20df45b06322b9393c93f
黑 <b@taboo.sh>
2024-10-01 17:39:10 +0800

    [6.2.0][dev] 修复 @Awake 在缺少参数时直接报错的问题

--- common-platform-api/src/main/kotlin/taboolib/common/platform/ClassVisitorAwake.kt
+++ common-platform-api/src/main/kotlin/taboolib/common/platform/ClassVisitorAwake.kt
@@ -3,13 +3,18 @@ package taboolib.common.platform
 import org.tabooproject.reflex.ClassMethod
 import org.tabooproject.reflex.ReflexClass
 import taboolib.common.LifeCycle
-import taboolib.common.TabooLib
 import taboolib.common.inject.ClassVisitor
+import taboolib.common.platform.function.warning
 
 class ClassVisitorAwake(private val lifeCycle: LifeCycle) : ClassVisitor(0) {
 
     override fun visit(method: ClassMethod, owner: ReflexClass) {
-        if (method.getAnnotationIfPresent(Awake::class.java)?.enumName("value") == lifeCycle.name) {
+        val enumName = method.getAnnotationIfPresent(Awake::class.java)?.enumName("value", "") ?: return
+        if (enumName == "") {
+            warning("Missing parameter in @Awake annotation: ${owner.name}#${method.name}")
+            return
+        }
+        if (enumName == lifeCycle.name) {
             val instance = findInstance(owner)
             if (instance != null) {
                 method.invoke(instance)
