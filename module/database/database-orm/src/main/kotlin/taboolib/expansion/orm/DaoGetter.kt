8fb204d26c7d3e6bf1c4403a1539af4148f10a6b
FxRayHughes <bushuaichen@yeah.net>
2024-10-11 12:12:33 +0800

    [Easy-ORM] ORMLite的包装框架 1. 对Getter的设计进行改良

--- module/database/database-orm/src/main/kotlin/taboolib/expansion/orm/DaoGetter.kt
+++ module/database/database-orm/src/main/kotlin/taboolib/expansion/orm/DaoGetter.kt
@@ -9,7 +9,9 @@ import kotlin.reflect.KProperty
 class DaoGetter<K, ID>(val entity: Class<K>, val id: Class<ID>) {
 
     // 拎出来进行加载 节省性能
-    val value = EasyORM.dao[entity.name] as Dao<K, ID>
+    val value by lazy {
+        EasyORM.dao[entity.name] as Dao<K, ID>
+    }
 
     operator fun getValue(ref: Any?, property: KProperty<*>): Dao<K, ID> {
         return value
