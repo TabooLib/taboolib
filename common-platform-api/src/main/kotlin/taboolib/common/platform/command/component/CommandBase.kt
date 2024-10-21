3a49e581d05f886ce68445ec20f127e5aa02f41a
黑 <b@taboo.sh>
2024-10-21 21:42:31 +0800

    [6.2.0] 补全内容不再限制以输入文本开头

--- common-platform-api/src/main/kotlin/taboolib/common/platform/command/component/CommandBase.kt
+++ common-platform-api/src/main/kotlin/taboolib/common/platform/command/component/CommandBase.kt
@@ -127,7 +127,7 @@ class CommandBase : CommandComponent(-1, false) {
                             else -> emptyList()
                         }
                     }
-                    suggest.filter { args.isEmpty() || it.startsWith(args, ignoreCase = true) }.ifEmpty { null }
+                    suggest.filter { args.isEmpty() }.ifEmpty { null }
                 }
                 else -> null
             }
